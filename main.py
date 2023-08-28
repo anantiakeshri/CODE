dc47a70b070bd473657c524df3bcb48f6d7a5193
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-29 00:39:11 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+""" BIG O notation"""
+
+# # 1. O(n)
+# def print_item(n):
+#     for i in range(n):
+#         print(i)
+        
+# print(print_item(10))
+
+
+# # 2. O(n^2)
+# def print_items(n):
+#     for i in range(n):
+#         for j in range(n):
+#             print(i, j)
+            
+# print(print_items(10))
+
+
+# 3. O(1) - constant time - most efficient
+def print_item(n):
+    return n + n + n
+
+print(print_item(10))
+
+# 4. O(log n) - more efficient than O(n) and O(n^2)
