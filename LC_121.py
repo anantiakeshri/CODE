9b7e2d24228fc1e015f56ec0d73322c3dacd2a6e
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-29 01:20:53 +0530

    Leet Code - Ques 121

---
+++ LC_121.py
@@ -0,0 +1,19 @@
+""" Best Time To Buy and Sell Stock """
+
+
+prices = [7,6,4,3,1]
+
+def maxProfit(prices):
+    max_Profit = 0
+    buy, sell = 0, 1
+
+    while sell < len(prices):
+        if prices[buy] < prices[sell]:
+            profit = prices[sell] - prices[buy]
+            max_Profit = max(max_Profit, profit)
+        else:
+            buy = sell
+        sell += 1
+    return max_Profit
+
+print(maxProfit(prices))
