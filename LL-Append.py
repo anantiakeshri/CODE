0602e632a3659aaaf8fc318ac4b8b5dfa5fa21fc
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-30 03:13:43 +0530

    Linked List Append

---
+++ LL-Append.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        # if no node is present in Linked List then add a node which is head and tail both.
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        
+        
+my_linked_list = LinkedList(1)
+my_linked_list.append(3)
+my_linked_list.append(18)
+my_linked_list.append(5)
+
+my_linked_list.print_list()
+        
+        
