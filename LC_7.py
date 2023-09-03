0ac41003cf9945581c041ae7b503d93c591cec1b
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-03 15:17:20 +0530

    Leet Code - Reverse Integer - Question 7

---
+++ LC_7.py
@@ -0,0 +1,22 @@
+# Reverse a Integer - Question 7
+
+# x = -123
+# Output: -321
+
+x = -123
+# Output: -321
+
+class Solution:
+    def reverse(x):
+        if x >= 0:
+            val = int(str(x)[::-1])
+        else:
+            val = -(int(str(abs(x))[::-1]))
+
+        if (val <= -2**31 or val >= 2**31 -1):
+            return 0
+        else:
+            return val
+        
+        
+    print(reverse(x))
