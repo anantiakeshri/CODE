37684e941e85ad808d4797d7992027536854c081
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-19 22:00:10 +0530

    Striver - 1.1.3

---
+++ 1.1.3.py
@@ -0,0 +1,12 @@
+# Striver - If Else (Decision Making) -  1.1.3
+
+from typing import *
+
+def compareIfElse(a: int, b: int):
+    # Write your code here
+    if a > b:
+        return "greater"
+    elif a < b:
+        return "Smaller"
+    else:
+        return "equal"
