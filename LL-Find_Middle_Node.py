bb090d4da597fd3e432a75c1590d851c9d23e273
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-24 21:31:20 +0530

    Find Middle Node in LL

---
+++ LL-Find_Middle_Node.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+        
+
+    # WRITE FIND_MIDDLE_NODE METHOD HERE #
+    def find_middle_node(self):
+        fast = self.head
+        slow = self.head
+        if self.head != None:
+            while (fast!= None and fast.next != None):
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+        return False
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+
+print( my_linked_list.find_middle_node().value )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    3
+    
+"""
