b77d2d3836cb82a73cb70ead955534cd9655493d
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-25 13:03:46 +0530

    LL Has Loop

---
+++ LL-Has_Loop.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    """ Approach 1"""
+    # def has_loop(self):
+    #     s = set()
+    #     temp = self.head
+    #     while (temp):
+    #         if (temp in s):
+    #             return True
+    #         s.add(temp)
+    #         temp = temp.next
+    #     return False
+       
+       
+    """ Approach 2 """ 
+    def has_loop(self):
+        slow_p = self.head
+        fast_p = self.head
+        while(slow_p and fast_p and fast_p.next):
+            slow_p = slow_p.next
+            fast_p = fast_p.next.next
+            if slow_p == fast_p:
+                return True
+        return False
+
+    
+""" Test Case 1 """   
+my_linked_list_1 = LinkedList(1)
+my_linked_list_1.append(2)
+my_linked_list_1.append(3)
+my_linked_list_1.append(4)
+my_linked_list_1.tail.next = my_linked_list_1.head
+print(my_linked_list_1.has_loop() ) # Returns True
+
+
+""" Test Cse 2 """
+my_linked_list_2 = LinkedList(1)
+my_linked_list_2.append(2)
+my_linked_list_2.append(3)
+my_linked_list_2.append(4)
+print(my_linked_list_2.has_loop() ) # Returns False
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    True
+    False
+    
+"""
