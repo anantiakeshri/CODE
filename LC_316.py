4427248b4c49fa23b861853f9286f03f6f46bfae
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-26 15:15:51 +0530

    Remove Duplicates - Leet Code - Ques 316

--- LC_316.py
+++ LC_316.py
@@ -1,16 +1,28 @@
-def removeDuplicateLetters(self, s: str) -> str:
-        result = []        
-        seen = set()
+# Leet Code - Ques 316
 
-        rightmost = {char: i for i, char in enumerate(s)}
-        
-        for i, char in enumerate(s):
-            if char not in seen:
-                while result and char < result[-1] and i < rightmost[result[-1]]:
-                    removed_char = result.pop()
-                    seen.remove(removed_char)
+s = "cbacdcbc"
+# Output: "acdb"
 
-                result.append(char)
-                seen.add(char)
-        
-        return ''.join(result)
+def removeDuplicateLetters(s: str) -> str:
+    # Initialize a stack to build the result.
+    stack = []
+    # Initialize a set to keep track of characters in the stack.
+    hash = set()
+    # Create a dictionary to store the last index of each character in the string.
+    last_occ = {char: i for i, char in enumerate(s)}
+
+
+    for i, char in enumerate(s):
+        if char not in hash:
+            # If the character is not in the stack and the last occurrence of the character
+            # is greater than the current index, pop characters from the stack until conditions are met.
+            while stack and char < stack[-1] and i < last_occ[stack[-1]]:
+                hash.discard(stack.pop())
+            # Add the current character to the stack and the set.
+            hash.add(char)
+            stack.append(char)
+            
+    # Convert the stack to a string to get the final result.
+    return ''.join(stack)
+
+print(removeDuplicateLetters(s))
