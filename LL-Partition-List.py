8195532eeb137e164116a4588238ff95c294f58d
anantiakeshri <476cs18007.rp@gmail.com>
2023-09-28 22:32:38 +0530

    LL Partition List

--- LL-Partition-List.py
+++ LL-Partition-List.py
@@ -82,14 +82,15 @@ ll.append(8)
 ll.append(10)
 ll.append(2)
 ll.append(1)
+ll.append(9)
 
 print("LL before partition_list:")
-ll.print_list() # Output: 3 5 8 10 2 1
+ll.print_list() # Output: 3 5 8 10 2 1 9
 
 ll.partition_list(5)
 
 print("LL after partition_list:")
-ll.print_list() # Output: 3 2 1 5 8 10
+ll.print_list() # Output: 3 2 1 5 8 10 9
 
 
 """
