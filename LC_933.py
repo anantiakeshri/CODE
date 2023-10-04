1a45d1062a909b7c974ab4e749773a3749ad3127
anantiakeshri <476cs18007.rp@gmail.com>
2023-10-04 23:29:59 +0530

    Leet Code - Ques 933 - Number of recent calls

---
+++ LC_933.py
@@ -0,0 +1,11 @@
+# Leet Code - Ques 933 - Number of recent calls
+from collections import deque
+class RecentCounter:
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        while self.queue[0] < t - 3000:
+            self.queue.popleft()
+        return len(self.queue)
