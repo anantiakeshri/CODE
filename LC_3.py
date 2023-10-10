d49405f4c917ebc7c413408c45f91d55218b5568
anantiakeshri <476cs18007.rp@gmail.com>
2023-10-10 14:32:43 +0530

    Leet Code - Ques 3

--- LC_3.py
+++ LC_3.py
@@ -3,22 +3,39 @@
 s = "pwwkew"
 # output - 3
 
+""" Approach 1 """
+# def lengthOfLongestSubstring(s: str) -> int:
+#     n = len(s)
+#     maxLength = 0
+#     charSet = set()
+#     left = 0
+    
+#     for right in range(n):
+#         if s[right] not in charSet:
+#             charSet.add(s[right])
+#             maxLength = max(maxLength, right - left + 1)
+#         else:
+#             while s[right] in charSet:
+#                 charSet.remove(s[left])
+#                 left += 1
+#             charSet.add(s[right])
+    
+#     return maxLength
+
+# print(lengthOfLongestSubstring(s))
+
+""" Approach 2 - O(n) - Sliding Window Technique"""
 def lengthOfLongestSubstring(s: str) -> int:
-    n = len(s)
-    maxLength = 0
     charSet = set()
-    left = 0
+    l = 0
+    res = 0
     
-    for right in range(n):
-        if s[right] not in charSet:
-            charSet.add(s[right])
-            maxLength = max(maxLength, right - left + 1)
-        else:
-            while s[right] in charSet:
-                charSet.remove(s[left])
-                left += 1
-            charSet.add(s[right])
-    
-    return maxLength
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        res = max(res, r - l + 1)
+    return res
 
 print(lengthOfLongestSubstring(s))
