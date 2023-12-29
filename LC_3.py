1d4c7265e912ad0ee074941060a88cbd1ba8e8b3
anantiakeshri <476cs18007.rp@gmail.com>
2023-12-29 19:55:24 +0530

    Leet Code 3

--- LC_3.py
+++ LC_3.py
@@ -1,31 +1,10 @@
 # Leet Code - Ques 3
 
-s = "pwwkew"
+s = "abcabcbcbb"
 # output - 3
 
-""" Approach 1 """
-# def lengthOfLongestSubstring(s: str) -> int:
-#     n = len(s)
-#     maxLength = 0
-#     charSet = set()
-#     left = 0
-    
-#     for right in range(n):
-#         if s[right] not in charSet:
-#             charSet.add(s[right])
-#             maxLength = max(maxLength, right - left + 1)
-#         else:
-#             while s[right] in charSet:
-#                 charSet.remove(s[left])
-#                 left += 1
-#             charSet.add(s[right])
-    
-#     return maxLength
-
-# print(lengthOfLongestSubstring(s))
-
-""" Approach 2 - O(n) - Sliding Window Technique"""
-def lengthOfLongestSubstring(s: str) -> int:
+# Sliding window technique
+def lengthOfLongestSubstring(s):
     charSet = set()
     l = 0
     res = 0
