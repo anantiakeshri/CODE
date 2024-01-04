3fe464bc87bb21bf88a522083fd4f945c2972161
anantiakeshri <476cs18007.rp@gmail.com>
2024-01-04 18:48:02 +0530

    Minimum Window Substring - Ques 76 - LeetCode

--- LC_76.py
+++ LC_76.py
@@ -4,37 +4,55 @@ s = "ADOBECODEBANC"
 t = "ABC"
 # Output: "BANC"
 
-def minWindow(s, t):
-    
-    if t == "" : return ""
+
+def minWindow(str1: str, str2: str) -> str:
+    if len(str1) < len(str2):
+        return ""
+
+    # using hashmaps
     countT, window = {}, {}
-    
-    for c in t:
-        countT[c] = 1 + countT.get(c, 0)    
-    have, need = 0, len(countT)
-    res, resLen = [-1, -1], float("infinity")
-    l = 0
-    
-    for r in range(len(s)):
-        c = s[r]
-        window[c] = 1 + window.get(c, 0)
-        
-        if c in countT and window[c] == countT[c]:
+
+    for c in str2:
+        countT[c] = 1 + countT.get(c, 0)
+        # counting chars in str2 
+
+    result = [-1,-1] # this is to save the result pointers
+    resLen = float("infinity")
+    have , need = 0 , len(countT)
+    # these variable are used to check if met the result or not 
+
+    l = 0 # left pointer, r is right pointer 
+    for r in range(len(str1)):
+        chr = str1[r] # character from str1 
+        # add this in hashmap ie window
+        window[chr] = 1 + window.get(chr, 0)
+
+        if chr in countT and window[chr] == countT[chr]:
             have += 1
-            
+        
+        # now if have == need means we got the result then we 
+        # start decreasing the result to get min from left side of result
+
         while have == need:
-            #update result
+            # update the result 
             if (r - l + 1) < resLen:
-                res = [l, r]
-                resLen = (r - 1 + 1)
-            #pop from left side of window
-            window[s[l]] -= 1
-            if s[l] in countT and window[s[l]] < countT[s[l]]:
-                have -= 1 
-            l += 1
+                result = [l, r]
+                resLen = (r - l + 1)
             
-    l, r = res
-    return s[l:r+1] if resLen != float("infinity") else ""
-    
-    
+            # decrease the length of the result, also we use while above 
+            # if after decreasing the length of result from left side and have is same 
+            # as need then we can update the result as we have to return min length
+
+            window[str1[l]] -= 1
+
+            # now checking if the character we removed is in need too
+            # then we have decrease the count of have as well to maintain it
+
+            if str1[l] in countT and window[str1[l]] < countT[str1[l]]:
+                have -= 1
+            l += 1
+        
+    l, r = result
+    return str1[l:r+1] if resLen != float("infinity") else ""
+
 print(minWindow(s, t))
