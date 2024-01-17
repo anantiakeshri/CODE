3aebde61a41ca7ebc3debe737a9357aa8f6af134
anantiakeshri <476cs18007.rp@gmail.com>
2024-01-17 23:01:31 +0530

    LC 88 - Merge Sorted Array

--- LC_88.py
+++ LC_88.py
@@ -1,13 +1,16 @@
-# Leet Code - Ques 88
+# Leet Code - Ques 88 - Merge Sorted Array
 
-nums1 = [0] 
-m = 0
-nums2 = [1]
-n = 1
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+# Output: [1,2,2,3,5,6]
 
 def mergeArray(nums1, m, nums2, n):
-    for i in range(m,m+n):
+    for i in range(m, m + n):
         nums1[i] = nums2[i-m]
     nums1.sort()
     
-print(mergeArray(nums1, m, nums2, n))
+# Calling of function
+mergeArray(nums1, m, nums2, n)
+print(nums1)
